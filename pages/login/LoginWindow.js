344bbf0d9f7d29d64ec7824421c1942b5b8400d3
i-figueroa <=>
2022-09-11 22:25:58 -0700

    Code refactor

--- pages/login/LoginWindow.js
+++ pages/login/LoginWindow.js
@@ -1,22 +1,15 @@
-import React from 'react';
-import GoogleIcon from '../../assets/Google_G.svg';
-import { compose } from 'recompose';
-import { connect } from 'react-redux';
+import GoogleIcon from '../../assets/Google_G.svg'
+import Image from 'next/image'
 
 const LoginWindow = ({ handleLogin }) => {
-
-    return (
-        <div className='login-window'>
-            <button className='login-google-button' type='button' onClick={handleLogin}>
-                <img src={GoogleIcon} alt='Google G'/>
-                <span>Login With Google</span>
-            </button>
-        </div>
-    )   
+	return (
+		<div className='login-window'>
+			<button className='login-google-button' type='button' onClick={handleLogin}>
+				<Image src={GoogleIcon} alt='Google G'/>
+				<span>Login With Google</span>
+			</button>
+		</div>
+	)   
 }
 
-const enhance = compose(
-    connect()
-);
-
-export default enhance(LoginWindow);
+export default LoginWindow;
--- pages/login/index.js
+++ pages/login/index.js
@@ -1,17 +1,9 @@
-import React from 'react';
-import { compose } from 'recompose';
-import { connect } from 'react-redux';
-import { login } from '../../flux/actions/authActions';
-import { isEmpty } from 'react-redux-firebase';
-import { Redirect } from 'react-router-dom';
-import { withLastLocation } from 'react-router-last-location';
-import LoginWindow from './LoginWindow';
+import LoginWindow from './LoginWindow'
 
 const LoginPage = ({auth, error, lastLocation, handleLogin}) => {
     // Check if users is authenticated, if not redirect to previous location or home
     if (!isEmpty(auth)) {
-        const redirectPath = lastLocation ? lastLocation : '/';
-        return <Redirect to={redirectPath}/>
+      console.log('not authorized')
     }
 
     return (
@@ -21,16 +13,4 @@ const LoginPage = ({auth, error, lastLocation, handleLogin}) => {
     )   
 }
 
-const enhance = compose(
-    connect(
-        state => ({
-            auth: state.firebase.auth,
-        }), 
-        dispatch => ({
-            handleLogin: () => dispatch(login()),
-        })
-    ),
-    withLastLocation
-);
-
-export default enhance(LoginPage);
+export default LoginPage
